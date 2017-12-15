2e8f2e6fbf6ba807262c452e9365ef930c0ff731
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-12-15 14:49:34 -0500

    1

--- OrcMathGUI/src/guiPlayer/Jas.java
+++ OrcMathGUI/src/guiPlayer/Jas.java
@@ -3,18 +3,23 @@ package guiPlayer;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
+import guiTeacher.components.AnimatedComponent;
 import guiTeacher.components.Component;
 
-public class Jas extends Component {
+public class Jas extends AnimatedComponent {
 
 	public Jas() {
-		super(40, 40, 100, 100);
+		super(40, 40, 200, 150);
+		addSequence("resources/dog.png", 100, 0, 66, 89, 54, 6);
+		Thread animation = new Thread(this);
+		animation.start();
 		update();
 	}
 
 	public void update(Graphics2D g) {
-		g.setColor(Color.GRAY);
-		g.fillRect(0, 0, getWidth(), getHeight());
+//		g.setColor(Color.GRAY);
+//		g.fillRect(0, 0, getWidth(), getHeight());
+		super.update(g);
 	}
 
 }
