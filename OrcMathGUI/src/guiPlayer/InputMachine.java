b720ac9e8ab571e306b3d22b0b7a0b7f93c72640
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-12-19 14:50:07 -0500

    v 2.2 no work as good

---
+++ OrcMathGUI/src/guiPlayer/InputMachine.java
@@ -0,0 +1,14 @@
+package guiPlayer;
+
+public class InputMachine {
+
+	public InputMachine() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
